cebf2e0966642f205e9cf0522288bd64983b1a03
Wicje <anichisom4top@gmail.com>
2025-08-29 20:28:38 +0000

    major refactord

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import helmet from "helmet";
+import cors from 'cors';
+import session from 'express-session';
+import rateLimit from 'express-rate-limit';
 import { connectDB } from "./lib/db.lib.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -16,6 +19,9 @@ const PORT = process.env.PORT;
 //Middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({}));
+app.use(cors());
+app.use(helmet());
 
 app.use("/api/auth", authRoutes);
 
